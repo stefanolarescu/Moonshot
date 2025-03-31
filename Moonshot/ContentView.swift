95654b78b89248c2332c5b3b1f244e0fafc92c7d
Stefan Olarescu <stefanolarescu@gmail.com>
2025-03-31 18:45:59 +0300

    Created Xcode project.

---
+++ Moonshot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Stefan Olarescu on 31.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Stefan Olarescu on 31.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
